3ef7b1ef5a3068dcc96171658f5461465c2dbbce
James Maniscalco <maniscalco.jt@gmail.com>
2019-02-02 17:34:13 -0500

    display recent moisture data in plot

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
-import os
+import os, numpy
 
-from flask import Flask
+from flask import Flask, render_template
 from werkzeug.contrib.fixers import ProxyFix
+
+from . import db
 
 
 def create_app():
@@ -16,16 +18,35 @@ def create_app():
     except OSError:
         pass
 
-    # a simple page that says hello
     @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    def home():
+        # get the last hour of data
+        from . import db
+        d = db.get_db()
+        sensors = d.execute('select * from sensors').fetchall()
+        time_data = []
+        moisture_data = []
+        sensor_names = []
+        for sensor in sensors:
+            db_output = d.execute("select timestamp, voltage from readings where sensor_id={} and timestamp > datetime('now', '-5 day')".format(sensor[0])).fetchall()
+            raw_data = numpy.array(db_output).transpose()
+            # limit to 100 lines or fewer
+            if len(raw_data[0]) > 1000:
+                idx = numpy.round(numpy.linspace(0, len(raw_data[0]) - 1, 100)).astype(int)
+            else:
+                idx = numpy.arange(len(raw_data[0]))
+            time_data.append(raw_data[0,idx].tolist())
+            # moisture = (voltage - low_limit)/(high_limit - low_limit)
+            m = (numpy.array([float(mm) for mm in raw_data[1,idx]]) - sensor[3]) / (sensor[2] - sensor[3]) * 100  # convert to percentage
+            moisture_data.append(m.tolist())
+            sensor_names.append('sensor {}'.format(sensor[0]))
+        return render_template("home.html", time_data=time_data, moisture_data=moisture_data, sensor_names=sensor_names)
 
     # WSGI setup
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
     # database connections
-    from . import db
+#    from . import db
     db.init_app(app)
 
     return app
